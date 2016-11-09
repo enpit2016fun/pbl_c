eb2b85aebd7e9c1fdce3a6bdc7c947f8e3a0a2d7
twatanabe88 <s1210147.oliva@gmail.com>
2016-11-09 23:47:35 +0900

    develop UIview

---
+++ Fengshui/UploadController.swift
@@ -0,0 +1,35 @@
+//
+//  UploadController.swift
+//  Fengshui
+//
+//  Created by 渡邊翼 on 2016/11/04.
+//  Copyright © 2016年 渡邊翼. All rights reserved.
+//
+
+import UIKit
+
+class UploadController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
