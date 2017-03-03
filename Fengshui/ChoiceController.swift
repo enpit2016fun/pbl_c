7569fd1873803c05478ab595846ab728ba86d471
b1012136 <g2116026@fun.ac.jp>
2017-03-03 11:52:32 +0900

    ランチスクリーンと選択画面をipadようにしました

--- Fengshui/ChoiceController.swift
+++ Fengshui/ChoiceController.swift
@@ -16,25 +16,32 @@ class ChoiceController: UIViewController,UIGestureRecognizerDelegate {
   var roomView: UIImageView!
   var fortuneView: UIImageView!
   var titleImage = UIImage(named:"ホーム帯.jpg")
-  var furnitureImage = UIImage(named:"ホーム画面：家具から占う.jpg")
-  var roomImage = UIImage(named:"ホーム画面：部屋から占う.jpg")
-  var fortuneImage = UIImage(named:"ホーム画面：運勢から占う.jpg")
+  var furnitureImage = UIImage(named:"ソファー.png")
+  var roomImage = UIImage(named:"家.png")
+  var fortuneImage = UIImage(named:"水晶.png")
+  var furnitureBackImage = UIImage(named: "家具から占う背景.png")
+  var roomBackImage = UIImage(named: "部屋から占う背景.png")
+  var fortuneBackImage = UIImage(named: "運勢から占う背景.png")
   var roomLabel: UILabel!
   var fortuneLabel: UILabel!
   var furnitureLabel: UILabel!
   var fortuneArray:[Fortune] = []
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = UIColor.whiteColor()
     setupFortune()
-    titleView = UIImageView(frame: CGRect(x: 0 , y:self.view.bounds.height * 0.04  , width: self.view.bounds.width, height: self.view.bounds.height * 0.15))
+    
+    /*ホーム帯*/
+    titleView = UIImageView(frame: CGRect(x: 0 , y: 0 , width: self.view.bounds.width, height: self.view.bounds.height * 0.16))
     // UIImageViewに画像を設定する.
     titleView.image = titleImage
     self.view.addSubview(titleView)
     
-    furnitureView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.73, width: self.view.bounds.width, height: self.view.bounds.height * 0.27))
+    /*家具から占う*/
+    furnitureView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.72, width: self.view.bounds.width, height: self.view.bounds.height * 0.28))
     // UIImageViewに画像を設定する.
-    furnitureView.image = furnitureImage
+    furnitureView.image = furnitureBackImage
     
     furnitureView.userInteractionEnabled = true
     
@@ -43,11 +50,25 @@ class ChoiceController: UIViewController,UIGestureRecognizerDelegate {
     furnitureTap.cancelsTouchesInView = false
     furnitureView.addGestureRecognizer(furnitureTap)
     self.view.addSubview(furnitureView)
-
     
-    fortuneView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.46, width: self.view.bounds.width, height: self.view.bounds.height * 0.27))
+    //家具から占うアイコンを表示
+    furnitureView = UIImageView(frame: CGRect(x: self.view.bounds.width * 0.28 , y: self.view.bounds.height * 0.8, width: self.view.bounds.width * 0.20, height: self.view.bounds.height * 0.11))
+    furnitureView.image = furnitureImage
+    self.view.addSubview(furnitureView)
+    //家具から占う文字を表示
+    furnitureLabel = UILabel(frame: CGRectMake(0,0,300,50))
+    furnitureLabel.layer.position = CGPoint(x: self.view.bounds.width * 0.72,y: self.view.bounds.height * 0.85)
+    furnitureLabel.text = "家具から占う"
+    furnitureLabel.textColor = UIColor.whiteColor()
+    furnitureLabel.font = UIFont(name:"HiraKakuProN-W3",size:27)
+    self.view.addSubview(furnitureLabel)
+    
+
+    /*運勢から占う*/
+    fortuneView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.44, width: self.view.bounds.width, height: self.view.bounds.height * 0.28))
     // UIImageViewに画像を設定する.
-    fortuneView.image = fortuneImage
+    fortuneView.image = fortuneBackImage
+    //fortuneView.image = fortuneImage
     
     fortuneView.userInteractionEnabled = true
     
@@ -56,10 +77,25 @@ class ChoiceController: UIViewController,UIGestureRecognizerDelegate {
     fortuneTap.cancelsTouchesInView = false
     fortuneView.addGestureRecognizer(fortuneTap)
     self.view.addSubview(fortuneView)
-    roomView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.19, width: self.view.bounds.width, height: self.view.bounds.height * 0.27))
-    // UIImageViewに画像を設定する.
-    roomView.image = roomImage
     
+    //運勢から占うアイコンの表示
+    fortuneView = UIImageView(frame: CGRect(x: self.view.bounds.width * 0.30 , y: self.view.bounds.height * 0.5, width: self.view.bounds.width * 0.16, height: self.view.bounds.height * 0.15))
+    fortuneView.image = fortuneImage
+    self.view.addSubview(fortuneView)
+    //運勢から占う文字表示
+    fortuneLabel = UILabel(frame: CGRectMake(0,0,300,50))
+    fortuneLabel.layer.position = CGPoint(x: self.view.bounds.width * 0.72,y: self.view.bounds.height * 0.58)
+    fortuneLabel.text = "運勢から占う"
+    fortuneLabel.textColor = UIColor.whiteColor()
+    fortuneLabel.font = UIFont(name:"HiraKakuProN-W3",size:27)
+    self.view.addSubview(fortuneLabel)
+    
+
+    /*部屋から占う*/
+    roomView = UIImageView(frame: CGRect(x: 0 , y: self.view.bounds.height * 0.16, width: self.view.bounds.width, height: self.view.bounds.height * 0.28))
+    // UIImageViewに画像を設定する.
+    roomView.image = roomBackImage
+    //roomView.image = roomImage
     
     roomView.userInteractionEnabled = true
     
@@ -68,8 +104,32 @@ class ChoiceController: UIViewController,UIGestureRecognizerDelegate {
     roomTap.cancelsTouchesInView = false
     roomView.addGestureRecognizer(roomTap)
     self.view.addSubview(roomView)
+    
+    //部屋から占うアイコン表示
+    roomView = UIImageView(frame: CGRect(x: self.view.bounds.width * 0.28 , y: self.view.bounds.height * 0.23, width: self.view.bounds.width * 0.2, height: self.view.bounds.height * 0.13))
+    roomView.image = roomImage
+    self.view.addSubview(roomView)
+    //部屋から占う文字表示
+    roomLabel = UILabel(frame: CGRectMake(0,0,300,50))
+    roomLabel.layer.position = CGPoint(x: self.view.bounds.width * 0.72,y: self.view.bounds.height * 0.30)
+    roomLabel.text = "部屋から占う"
+    roomLabel.textColor = UIColor.whiteColor()
+    roomLabel.font = UIFont(name:"HiraKakuProN-W3",size:27)
+    self.view.addSubview(roomLabel)
+    
     // Do any additional setup after loading the view.
+    
+    //実機の画面のお大きさチェック
+    print("width: \(self.view.bounds.width)")
+    print("height: \(self.view.bounds.height)")
+    
+    //縦横比対応
+    if(self.view.bounds.width == 1024 && self.view.bounds.height == 1366){
+        print("test")
+    }
   }
+    
+    
   
   func furnitureTapAction(sender: UITapGestureRecognizer){
     // 遷移するViewを定義する.
